31fa198478a7db5bb456b3c98ed788de600489c3
Ryan Graham <53840442+ry-gr@users.noreply.github.com>
2026-05-03 08:35:29 -0700

    Update manual_trigger_apr24_with_relays.ino

--- arduinoFiles/manual_trigger_apr24_with_relays.ino
+++ arduinoFiles/manual_trigger_apr24_with_relays.ino
@@ -2,24 +2,17 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(4, OUTPUT); //trigger pin for charging relay
-  pinMode(3, OUTPUT); //power for charging relay
   pinMode(2, OUTPUT); //trigger pin for thyristor relay
-  pinMode(7, OUTPUT); //power for thyristor relay
 
-  digitalWrite(3, LOW);
   digitalWrite(4, LOW);
-  digitalWrite(7, LOW);
-  //the relays must be set low here otherwise they turn on unpredictably at arduino startup
-
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int del;
   if (Serial.available() > 0) 
   { 
-    digitalWrite(3, HIGH);//supplies power to charging relay
-    digitalWrite(7, HIGH);//supplies power to thyristor relay
     String input = Serial.readString();
     input.trim();
     Serial.println(input);
@@ -28,7 +21,7 @@ void loop() {
     {
       digitalWrite(4, HIGH); //turns on charging relay, letting capacitor start to charge
       Serial.println("charging cap");
-      int del = 18000; //how much should the capacitor charge? 
+      del = 20000; //how much should the capacitor charge? 
       for(int i = 0; i<4; i++)
       {
         delay(del/4);
@@ -45,7 +38,27 @@ void loop() {
       digitalWrite(2, LOW);
     }
 
+    if(input.equals("charge"))
+    {
+      digitalWrite(4, HIGH); //turns on charging relay, letting capacitor start to charge
+      Serial.println("charging cap");
+      del = 25000; //how much should the capacitor charge? 
+      for(int i = 0; i<4; i++)
+      {
+        delay(del/4);
+        Serial.print((25*(i+1)));
+        Serial.println("% of the way charged");
+        //simple charging progress loop
+      }
+      digitalWrite(4, LOW);
 
-    
+    }
+    if(input.equals("fire"))
+    {
+      digitalWrite(2, HIGH); //triggers thyristor relay allowing the thyristor to close and the coil to fire
+      Serial.println("discharged capacitor");
+      delay(10); //latches for 10ms
+      digitalWrite(2, LOW);
+    }
   }
 }
